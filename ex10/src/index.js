5c8e37a0190e891ad16ce609f7aefb521abd5542
zivojinminic <zivojinminic05@gmail.com>
2021-09-16 19:46:51 +0200

    String variables

---
+++ ex10/src/index.js
@@ -0,0 +1,15 @@
+function main(){
+    //only change code below this line
+    var myFirstName = 'Zivojin';
+    var myLastName = 'Minic';
+    var myHomeTown = 'Bijelo Polje';
+    //only change code above this line
+    return {
+        myFirstName,
+        myLastName,
+        myHomeTown
+    };
+    }
+
+console.log(main());
+module.exports = main;
