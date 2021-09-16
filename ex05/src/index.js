d6ce2abadfd6efd77cd1a27e2d6ab5e18763b1d6
zivojinminic <zivojinminic05@gmail.com>
2021-09-16 19:01:31 +0200

    Multiplying two decimals

---
+++ ex05/src/index.js
@@ -0,0 +1,13 @@
+function main(numOne, numTwo){
+    //only change code below this line
+    var numOne;
+    var numTwo;
+    var product = numOne * numTwo;
+    //only change code above this line
+    return product;
+}
+
+console.log(main(2.5, 2.0));        //Change this line
+console.log(main(5.5, 5.5));
+console.log(main(100.25, 43.20));
+module.exports = main;
