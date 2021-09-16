58d7ce047fafbcad27428536f60d448aac76334e
zivojinminic <zivojinminic05@gmail.com>
2021-09-16 18:58:13 +0200

    Decimal numbers

---
+++ ex04/src/index.js
@@ -0,0 +1,13 @@
+function main(){
+    //only change code below this line
+    var myDecimal = 8.2;
+    var myDecimalE = 254e-5;
+    //only change code above this line
+    return {
+        myDecimal,
+        myDecimalE
+    };
+    }
+
+console.log(main());
+module.exports = main;
