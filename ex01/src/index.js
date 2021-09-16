e66899239b5e1524b3b543dfd90fd800cf2363dd
zivojinminic <zivojinminic05@gmail.com>
2021-09-16 18:49:45 +0200

    Dividing numbers in javascript

---
+++ ex01/src/index.js
@@ -0,0 +1,20 @@
+function main(numOne, numTwo){
+    var quotient;
+    if(numTwo === 0){
+        quotient = 'Divide by 0! Cannot divide by zero!';
+    } else {
+        //only change code below this line
+        var numOne;
+        var numTwo;
+        var quotient = numOne / numTwo;
+        //only change code above this line
+    }
+
+    return quotient;
+}
+
+console.log(main(15, 3));      //change this line
+console.log(main(25, 5));
+console.log(main(420, 10));
+console.log(main(55, 0));
+module.exports = main;
