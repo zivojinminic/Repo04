63394eec1bb4710bff0c439570d4cc9088466dd2
zivojinminic <zivojinminic05@gmail.com>
2021-09-16 19:11:34 +0200

    Finding a remainder

---
+++ ex07/src/index.js
@@ -0,0 +1,13 @@
+function main(numOne, numTwo){
+    //only change code below this line
+    var numOne;
+    var numTwo;
+    var remainder = numOne % numTwo;
+    //only change code above this line
+    return remainder;
+    }
+
+console.log(main(5, 2));    //Change this line
+console.log(main(15, 4));
+console.log(main(102, 25));
+module.exports = main;
